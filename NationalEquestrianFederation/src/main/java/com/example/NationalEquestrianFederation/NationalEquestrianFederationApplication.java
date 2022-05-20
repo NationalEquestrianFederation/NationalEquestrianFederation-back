23f68de7443134528ec1105cfff2f3de8655deb5
poparicsara <poparic.ra60.2018@uns.ac.rs>
2022-05-20 12:31:12 +0200

    feat: initial project

---
+++ NationalEquestrianFederation/src/main/java/com/example/NationalEquestrianFederation/NationalEquestrianFederationApplication.java
@@ -0,0 +1,13 @@
+package com.example.NationalEquestrianFederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalEquestrianFederationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalEquestrianFederationApplication.class, args);
+	}
+
+}
